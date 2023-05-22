41abfd92fdc1f4786f3bd058d8a820d94f79cfe2
Amala Shwetah <93658002+Amala02@users.noreply.github.com>
2023-05-22 08:51:06 +0530

    API_added

---
+++ backend/api.py
@@ -0,0 +1,77 @@
+from flask import Flask,request,jsonify,render_template
+from flask import Flask, redirect, url_for, request
+import pickle
+import tensorflow as tf
+from numpy import argmax
+import numpy as np
+import json
+
+app=Flask(__name__)
+import transformers
+model=tf.keras.models.load_model('model.h5',custom_objects={"TFBertModel": transformers.TFBertModel})
+lst=[]
+
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+
+
+
+@app.route('/predict/<string:msg>' ,methods=["POST"])
+def predict(msg):
+  
+        string_text= json.loads(msg)
+        print(string_text)
+        tokenized_text= tokenizer(
+        text=string_text,
+        add_special_tokens=True,
+        max_length=70,
+        truncation=True,
+        padding='max_length', 
+        return_tensors='tf',
+        return_token_type_ids = False,
+        return_attention_mask = True,
+        verbose = True)
+        result= model.predict({'input_ids':tokenized_text['input_ids'],'attention_mask':tokenized_text['attention_mask']})
+        output = argmax(result[0])
+        lst.append(output)
+        print(output)
+        print(lst)
+        
+    
+
+
+
+
+
+
+
+
+@app.route('/suggest/<string:msg>' ,methods=["POST"])
+def suggest(msg):
+            string_text= json.loads(msg)
+            global lst2 
+            lst2 = np.array(lst)
+            if ((lst2==1).sum())>((lst2==2).sum() or (lst2==3).sum() or (lst2==4).sum() or (lst2==5).sum()):
+                pred="calm"
+            elif ((lst2==0).sum())<((lst2==4).sum()):
+                pred="sad but angry"
+            elif ((lst2==0).sum())>((lst2==4).sum()):
+                pred="calm songs"
+            elif ((lst2==2).sum())>0 and ((lst2==3).sum())>0:
+                pred="happy love"
+            elif ((lst2==3).sum())>0 and ((lst2==4).sum())>0:
+                pred="heartbreak"
+            elif ((lst2==2).sum())>0 and ((lst2==5).sum())>0:
+                pred="chill"
+           
+            else:
+                pred="random"
+
+            
+
+            return{"pred":pred}
+
+
+
+
+
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,45 @@
 import React from "react";
 import  { useState, useEffect } from 'react';
 import { ReactDOM } from "react";
-
+import axios from "axios";
 import './Home.css';
+
+
+
+
 export default function Home(){
+	const [musicData, setMusic] = useState(0)
+	function suggest() {
+		var msg=document.getElementById("message").value
+		const request = new XMLHttpRequest()
+		request.open('POST', `/suggest/${JSON.stringify(msg)}`)
+		request.send();
+		axios({
+		  method: "POST",
+		  url:"/suggest",
+		})
+		.then((response) => {
+		  let res =response.data
+		  setMusic(({
+			music: res.pred
+			}))
+		})}
 	
-	
+		function predict() {
+			var msg=document.getElementById("message").value
+			const request = new XMLHttpRequest()
+			request.open('POST', `/predict/${JSON.stringify(msg)}`)
+			request.send();
+			axios({
+			  method: "POST",
+			  url:"/predict",
+			})
+			.then((response) => {
+			  const res =response.data
+			  setMusic(({music:0
+				
+				}))
+			})}
 	return(
 
 		<div id="home-wrap">
@@ -14,10 +48,30 @@ export default function Home(){
 			</div>
 			<div id="content">
 				<div id="chat-bot">
-					ChatBot
+				
+    				<form method="POST">
+						<p id="text">Enter Your Text Here</p>
+						<textarea id="message" rows="6" cols="50"></textarea>
+						<br/>
+
+						<input type="submit" class="btn-info" value="predict" onClick={predict}></input>
+		
+					</form>
+					<form method="POST">
+					<br/>
+						<input type="submit" class="btn-info" value="suggest" onClick={suggest}></input>
+		
+					</form>
+
+				
 				</div>
 				<div id="music-player">
 					MusicPlayer
+					<p>{musicData.music}</p>
+              			
+              
+            	
+        
 				</div>
 			</div>
 			<div id='footer'>
